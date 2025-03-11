d06b7452149652d3113cfbc72f9147e1ab1d292f
Dasteeny <dastan.zhabagin@gmail.com>
2025-03-11 09:17:03 +0500

    ✨ Implement Maze class for grid management and drawing functionality

---
+++ maze.py
@@ -0,0 +1,51 @@
+import time
+
+from cell import Cell
+from graphics import Window
+
+
+class Maze:
+    def __init__(
+        self,
+        x1: int,
+        y1: int,
+        num_rows: int,
+        num_cols: int,
+        cell_size_x: int,
+        cell_size_y: int,
+        win: Window,
+    ):
+        self._cells: list[list[Cell]] = []
+        self._x1: int = x1
+        self._y1: int = y1
+        self._num_rows: int = num_rows
+        self._num_cols: int = num_cols
+        self._cell_size_x: int = cell_size_x
+        self._cell_size_y: int = cell_size_y
+        self._win: Window = win
+
+        self._create_cells()
+
+    def _create_cells(self):
+        for i in range(self._num_rows):
+            col_cells = []
+            for j in range(self._num_cols):
+                col_cells.append(Cell(self._win))
+            self._cells.append(col_cells)
+
+        for i in range(self._num_rows):
+            for j in range(self._num_cols):
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i: int, j: int):
+        x1 = self._x1 + self._cell_size_x * j
+        y1 = self._y1 + self._cell_size_y * i
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+
+    def _animate(self):
+        self._win.redraw()
+        time.sleep(0.05)
